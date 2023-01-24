c09c49ceb78ccdf029da63de6a6e8fa0fdebcf45
Dmitr Cambur <dmitr.cambur@student.ehb.be>
2023-01-24 16:34:09 +0100

    ARTIST + ARTWORK PAGE

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 "use strict"
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+let heart = document.querySelector('#heart');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
+}
+
+heart.onclick = () => {
+    heart.classList.toggle('ri-heart-fill');
 }
