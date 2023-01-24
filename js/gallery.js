c205f43fb77b14e95b5bf739f9de1f4292f34e6d
AachalShrestha <aachal.shrestha@student.ehb.be>
2023-01-24 13:02:46 +0100

    toggle arrows, diplay and hide filters

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,5 @@
+const toggleArrows = document.querySelectorAll('.filter__section__title');
+
 document.getElementById('filterButton').addEventListener('click', (e) => {
     e.preventDefault();
     document.getElementById('filterSlideDiv').style.right = "0%";
@@ -5,3 +7,13 @@ document.getElementById('filterButton').addEventListener('click', (e) => {
 document.getElementById('goBackButton').addEventListener('click', (e) => {
     document.getElementById('filterSlideDiv').style.right = "-100%";
 })
+
+//rotate arrows
+toggleArrows.forEach(button => {
+    console.log(button.children);
+    button.addEventListener('click', () => {
+        console.log('arrow clicked');
+        button.children[1].classList.toggle("bx-chevron-left--on");
+        button.nextElementSibling.classList.toggle("filter__section__filters--selected")
+    });
+});
