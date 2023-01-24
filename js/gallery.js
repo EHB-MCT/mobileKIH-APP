fad4b5cee91ba8a052428a4dd077f9b33772cdd6
AachalShrestha <aachal.shrestha@student.ehb.be>
2023-01-24 16:25:09 +0100

    selecter filter add to page

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,11 @@
 const toggleArrows = document.querySelectorAll('.filter__section__title');
 
+const filters = {
+    type: [],
+    artist: [],
+    subject: []
+}
+
 document.getElementById('filterButton').addEventListener('click', (e) => {
     e.preventDefault();
     document.getElementById('filterSlideDiv').style.right = "0%";
@@ -17,3 +23,51 @@ toggleArrows.forEach(button => {
         button.nextElementSibling.classList.toggle("filter__section__filters--selected")
     });
 });
+
+document.querySelectorAll('.filter__section__filters').forEach(section => {
+    section.addEventListener('click', (e) => {
+
+        if (e.target.id == "type" || e.target.id == "artist" || e.target.id == "subject") {} else {
+            let array = section.id;
+            console.log(e.target.id);
+            if (filters[array].includes(e.target.id)) {
+                let index = filters[array].indexOf(e.target.id);
+                filters[array].splice(index, 1);
+
+            } else {
+                filters[array].push(e.target.id);
+            }
+            console.log(filters);
+            document.getElementById(e.target.id).classList.toggle("filter__section__filters--active");
+
+        }
+
+    });
+});
+
+
+//appplyin filters
+document.getElementById('filter_applyButton').addEventListener("click", () => {
+    const filtersDiv = document.getElementById('selectedFilters');
+    filtersDiv.innerHTML = '';
+    for (const array in filters) {
+        filters[array].forEach(tag => {
+
+            filtersDiv.innerHTML += `<div class="selectedFilters__filter" id="${tag}">
+            <p>${tag}</p>
+            <i class='bx bx-x selectedFilter__filter__cross'></i>
+        </div>`;
+        })
+    }
+
+    document.getElementById('filterSlideDiv').style.right = "-100%";
+})
+
+//removing a filter
+
+document.querySelectorAll('.selectedFilter__filter__cross').forEach(cross => {
+    cross.addEventListener('click', (e) => {
+        console.log(e.target.id)
+        document.getElementById(e.target.id).remove();
+    })
+})
