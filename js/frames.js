0053e01a166960fa0b97dbbe69f57ac145594bbd
AachalShrestha <aachal.shrestha@student.ehb.be>
2023-01-25 11:28:32 +0100

    frames page, js added

--- js/frames.js
+++ js/frames.js
@@ -18,3 +18,27 @@ toggleArrows.forEach(button => {
 });
 
 //add room
+
+let PREVIOUSLY_CHOSEN = '';
+let CHOSEN_ROOM;
+
+document.querySelectorAll('.choice__existingRooms').forEach(room => {
+    room.addEventListener('click', (e) => {
+        if (CHOSEN_ROOM == e.target.id) {
+            CHOSEN_ROOM = '';
+        } else {
+            PREVIOUSLY_CHOSEN = CHOSEN_ROOM;
+            CHOSEN_ROOM = e.target.id;
+            document.getElementById(e.target.id).parentElement.classList.toggle('choice__existingRooms__room--selected');
+        }
+        console.log(CHOSEN_ROOM);
+
+    });
+});
+
+
+// add new room
+document.getElementById('plus_addroom').addEventListener('click', () => {
+    document.getElementById('plus_addroom').classList.toggle('plus--active')
+    document.getElementById('addRoom-wrapper').classList.toggle('addRoom-wrapper--active');
+})
