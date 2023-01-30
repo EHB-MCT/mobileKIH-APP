142b26441b53b4cf04ace1298cf9e27279ee0164
Dmitr Cambur <dmitr.cambur@student.ehb.be>
2023-01-30 13:06:52 +0100

    profile page adjusted
    -ali fix profile icon and rent button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 "use strict"
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let heart = document.querySelector('#heart');
+
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
 
-heart.onclick = () => {
-    heart.classList.toggle('ri-heart-fill');
-}
