619f6b87e4346ac13a8db2c3309f5359c26a413c
Ali <ali.amrani@student.ehb.be>
2023-02-01 05:34:59 +0100

    Check box communication

--- src/diplayFrame.js
+++ src/diplayFrame.js
@@ -1,6 +1,7 @@
 const { io } = require('../node_modules/socket.io/client-dist/socket.io.js')
 var socket = io('https://dimetrondon-backend.onrender.com/');
 import Cookies from "js-cookie";
+import { keys } from "lodash";
 
 if (!Cookies.get('user')) window.location = './login.html'
 
@@ -17,7 +18,19 @@ slider.addEventListener('input', (e) => {
 
 fetch('https://dimetrondon-backend.onrender.com/getLikesOfuSER')
     .then(e => e.json())
-    .then(data => {
+    .then(async data => {
+        let fetchres = await fetch('https://dimetrondon-backend.onrender.com/getFrameSettings/'+screenid)
+
+        let result = await fetchres.json();
+        let settings = (JSON.parse(result[0][0].settings))
+        let keysToCheck =[]
+        document.getElementById('range').value = settings.interval;
+        document.getElementById('settime').innerText = settings.interval;
+
+        if(Object.keys(settings).length){
+            keysToCheck = settings.ids;
+        }
+        console.log(keysToCheck.includes(32))
         data[0].forEach((e, i) => {
 
 
@@ -27,6 +40,14 @@ fetch('https://dimetrondon-backend.onrender.com/getLikesOfuSER')
                 source.type = "video/mp4"
                 let test = document.createElement('video');
                 test.classList.add('test')
+                if(keysToCheck.includes(e.idart)){
+                    test.classList.add('chek')
+
+                }else{
+                    test.classList.add('che')
+
+                }
+                test.name = e.idart
                 test.autoplay = true;
                 test.loop = true;
                 test.muted = true;
@@ -43,13 +64,19 @@ fetch('https://dimetrondon-backend.onrender.com/getLikesOfuSER')
                 let blur = document.createElement('img');
                 test.classList.add('image')
                 blur.classList.add('blur')
+                if(keysToCheck.includes(e.idart)){
+                    test.classList.add('chek')
+
+                }else{
+                    test.classList.add('che')
+
+                }
+                test.name = e.idart
                 test.src = "https://dimetrodon.fr/files/" + e.file.split('.')[0] + '.jpg';
                 document.querySelectorAll('.column')[i % 2].appendChild(test)
 
                 test.addEventListener('click', () => {
                     emitToDisplay(e.idart);
-
-
                 })
 
 
@@ -59,6 +86,14 @@ fetch('https://dimetrondon-backend.onrender.com/getLikesOfuSER')
                 let blur = document.createElement('img');
                 test.classList.add('image')
                 blur.classList.add('blur')
+                if(keysToCheck.includes(e.idart)){
+                    test.classList.add('chek')
+
+                }else{
+                    test.classList.add('che')
+
+                }
+                test.name = e.idart
                 test.src = "https://dimetrodon.fr/files/" + e.file;
                 blur.src = "https://dimetrodon.fr/files/" + e.file;
                 test.addEventListener('click', () => {
@@ -69,10 +104,80 @@ fetch('https://dimetrondon-backend.onrender.com/getLikesOfuSER')
 
             }
         });
+        $(".che").imgCheckbox({
+            graySelected: false,
+            "styles": {
+                "span.imgCheckbox.imgChked": {
+                    "border":"0px solid #f3f0ec"
+                },
+                "span.imgCheckbox.imgChked img": {
+
+                    // Let's change the amount of scaling from the default of "0.8"
+                    "transform": "scale(0.9)"
+                },
+
+                "span.imgCheckbox.imgChked video": {
+                    // Let's change the amount of scaling from the default of "0.8"
+                    "transform": "scale(0.9)"
+                }
+            },
+            onclick: async function(el){
+                let ids = getSelected();   
+                let res = await postReq("https://dimetrondon-backend.onrender.com/updateSettings", {idframe: screenid,settings :JSON.stringify({ids: ids,interval: document.getElementById('range').value})})
+                console.log(res);
+            }
+        });
+        $(".chek").imgCheckbox({
+            graySelected: false,
+            preselect: true,
+            "styles": {
+                "span.imgCheckbox.imgChked": {
+                    "border":"0px solid #f3f0ec"
+                },
+                "span.imgCheckbox.imgChked img": {
+
+                    // Let's change the amount of scaling from the default of "0.8"
+                    "transform": "scale(0.9)"
+                },
+
+                "span.imgCheckbox.imgChked video": {
+                    // Let's change the amount of scaling from the default of "0.8"
+                    "transform": "scale(0.9)"
+                }
+            },
+            onclick: async function(el){
+                let ids = getSelected();   
+                let res = await postReq("https://dimetrondon-backend.onrender.com/updateSettings", {idframe: screenid,settings :JSON.stringify({ids: ids,interval: document.getElementById('range').value})})
+                console.log(res);
+            }
+        });
     })
 
 
 function emitToDisplay(display) {
     socket.emit('broadcast-dis', screenid + '-display+' + display)
 
+}
+
+
+function getSelected()  {
+    let all = document.querySelectorAll('.imgChked');
+    let idsList = [] 
+    all.forEach(e=>{idsList.push(parseInt(e.childNodes[0].name))});
+    console.log(idsList);
+    return idsList;
+};
+
+
+async function postReq(url, data) {
+
+    let resp = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    return await resp.json();
 }
